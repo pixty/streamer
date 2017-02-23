7b1b5f4c8b9fa08a73838fc147f21bf8ea01d07a
Dmitry Spasibenko <dmitry.spasibenko@mulesoft.com>
2017-02-23 15:10:08 -0800

    stream server

--- strmserver/streamer-srv.go
+++ strmserver/streamer-srv.go
@@ -3,14 +3,15 @@ package main
 import (
 	"flag"
 	"io"
-	//"io/ioutil"
+	"io/ioutil"
 	"math"
 	"net/http"
 	"os"
 	"path"
-	//"path/filepath"
+	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/jrivets/gorivets"
 	"github.com/jrivets/log4g"
@@ -22,18 +23,20 @@ type server struct {
 	storageDir  string
 	storMaxSize int64
 	port        int
+	lock        sync.Mutex
 }
 
 func main() {
 	defer log4g.Shutdown()
 
 	s := new(server)
-	var help bool
+	var help, debug bool
 	var storMaxSize string
 	flag.StringVar(&s.storageDir, "storage-dir", "./", "Where we are going to store data")
 	flag.StringVar(&storMaxSize, "max-size", "10Gb", "Where we are going to store data")
 	flag.IntVar(&s.port, "port", 8080, "port to listen")
 	flag.BoolVar(&help, "help", false, "Prints the usage")
+	flag.BoolVar(&debug, "debug", false, "Debug mode")
 
 	flag.Parse()
 	if help {
@@ -42,6 +45,9 @@ func main() {
 	}
 
 	s.logger = log4g.GetLogger("streamer")
+	if debug {
+		log4g.SetLogLevel("streamer", log4g.DEBUG)
+	}
 
 	maxSize, err := gorivets.ParseInt64(storMaxSize, 10000000, math.MaxInt64, 10000000)
 	if err != nil {
@@ -95,24 +101,63 @@ func (s *server) newImage(c *gin.Context) {
 		return
 	}
 
+	s.sweep()
 	c.Status(http.StatusCreated)
 }
 
 func (s *server) sweep() {
-	//	s.logger.Info("Sweeping...")
-	//	files, err := ioutil.ReadDir(s.storageDir)
-	//	if err != nil {
-	//		s.logger.Error("Could not read files from ", s.storageDir, ", cancel sweeping...")
-	//		return
-	//	}
+	s.lock.Lock()
+	defer s.lock.Unlock()
 
-	//	ss := gorivets.NewSortedSliceByComp(cmpStr, len(files))
-	//	for file := range files {
-	//		_, fn := filepath.Split(file.Name())
-	//		ext := strings.ToLower(filepath.Ext(fn))
+	s.logger.Info("Sweeping...")
+	defer s.logger.Info("Sweeping Done")
 
-	//		if
-	//	}
+	files, err := ioutil.ReadDir(s.storageDir)
+	if err != nil {
+		s.logger.Error("Could not read files from ", s.storageDir, ", cancel sweeping...")
+		return
+	}
+
+	ss, _ := gorivets.NewSortedSliceByComp(cmpStr, len(files))
+	var size int64 = 0
+	for _, file := range files {
+		_, fn := filepath.Split(file.Name())
+		ext := strings.ToLower(filepath.Ext(fn))
+
+		s.logger.Debug("Found file ", fn, " with ext=", ext)
+
+		if strings.HasPrefix(fn, "2017-") && ext == ".mp4" {
+			ss.Add(fn)
+			size += file.Size()
+		}
+	}
+
+	s.logger.Info(ss.Len(), " files found, total size is ", gorivets.FormatInt64(size, 1000))
+
+	if size > s.storMaxSize {
+		s.logger.Info("Folder size=", gorivets.FormatInt64(size, 1000), " is bigger than maximum value ", gorivets.FormatInt64(s.storMaxSize, 1000), " sweeping files...")
+		removed := 0
+		for size > s.storMaxSize && ss.Len() > 0 {
+			fn := ss.DeleteAt(0).(string)
+			fullFn := path.Join(s.storageDir, fn)
+
+			fi, err := os.Stat(fullFn)
+			if err != nil {
+				s.logger.Error("Could not get stat for ", fullFn, ", err=", err)
+				continue
+			}
+
+			s.logger.Info("Removing file ", fullFn, " ...")
+			err = os.Remove(fullFn)
+			if err != nil {
+				s.logger.Error("Could not remove file ", fullFn, ", err=", err)
+			} else {
+				removed++
+				size -= fi.Size()
+			}
+		}
+		s.logger.Info(removed, " files were removed, new folder size is ", gorivets.FormatInt64(size, 1000), "(", size, ")")
+	}
 }
 
 func cmpStr(a, b interface{}) int {
